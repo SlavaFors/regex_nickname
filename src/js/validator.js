2a40326d3fcbede9186dbef6e12101cae49377c9
SlavaFors <furtive7@yandex.ru>
2023-05-20 15:59:13 +0300

    'v2_2005'

--- src/js/validator.js
+++ src/js/validator.js
@@ -4,7 +4,30 @@ export default class Validator {
   }
 
   validateUsername() {
-    const re = /^[a-zA-Z]([a-zA-Z]|([_-])|([\d](?!\d{3})))*(?<![\d_-])$/;
-    return re.test(this.str);
+    const parameter1 = /^[a-z]+/i.test(this.str);
+    const parameter2 = /[a-z\-_\d]*/i.test(this.str);
+    const parameter3 = /[a-z]+$/i.test(this.str);
+
+    const param1 = !/\d{4,}/.test(this.str);
+    const param2 = !/[-_]+$/.test(this.str);
+    const param3 = !/[0-9]+$/.test(this.str);
+
+    return (parameter1 && parameter2 && parameter3 && param1 && param2 && param3);
   }
 }
+
+/* Допустимы:
+- только латинские буквы
+- символы тире -,
+- подчёркивания _
+- цифры (0-9);
+Имя не должно:
+- содержать подряд более трёх цифр
+- не должно начинаться и заканчиваться цифрами
+- не должно начинаться и заканчиваться символами подчёркивания
+- не должно начинаться и заканчиваться символами тире.
+/*validateUsername() {
+        const parameter1 = /^[a-z]+[a-z\-_\d]*[a-z]+$/i.test(this.str);
+        const parameter2 = !/\d{4,}/.test(this.str);
+        return (parameter1 && parameter2);
+} */
